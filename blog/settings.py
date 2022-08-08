ade41f52888d583eaf9033fbc3295df9649ae00a
Fábio Luiz Sousa <fabioprogramar@gmail.com>
2022-08-08 14:33:10 -0300

    db.json e configurações no settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = f'{secret_key}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-projeto.fabiols.com.br']
 
 
 # Application definition
